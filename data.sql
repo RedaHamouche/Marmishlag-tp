497e42bf20c8c89eb87d3f445f4e114b448dd3de
rqphy <raphael.ferreira@hetic.net>
2022-03-20 23:04:19 +0100

    yolo

--- data.sql
+++ data.sql
@@ -20,8 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `data`
 --
-CREATE DATABASE IF NOT EXISTS `data` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `data`;
+CREATE DATABASE IF NOT EXISTS `marmishlag` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `marmishlag`;
 
 -- --------------------------------------------------------
 
