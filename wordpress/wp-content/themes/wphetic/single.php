27e3e106f285a1b78511bece69a86017db80254b
Raphael <raphael.ferreira@hetic.net>
2022-04-04 11:53:29 +0200

    style article

--- wordpress/wp-content/themes/wphetic/single.php
+++ wordpress/wp-content/themes/wphetic/single.php
@@ -16,14 +16,14 @@
     <?php if (have_posts()) : ?>
         <?php while (have_posts()) : ?>
             <?php the_post(); ?>
-            <section class="card">
-                <h1></h1>
-                <h2><?php the_title(); ?></h2>
-                <p><?php the_excerpt(); ?></p>
-            </section>
+            <article class="article">
+                <h1><?php the_title(); ?></h1>
+                <p><?php the_content(); ?></p>
+            </article>
             
         <?php endwhile; ?>
     <?php endif; ?>
+    
 </main>
                 
 <?php get_footer(); ?>
